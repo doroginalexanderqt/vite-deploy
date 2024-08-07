fd32aff24de0d58e349fa9780a222cdf638e707d
Aliaksandr Darohin <aliaksandr.darohin@billingplatform.com>
2024-08-07 04:52:30 +0400

    reset initial values

--- src/components/CreditForm/validation.ts
+++ src/components/CreditForm/validation.ts
@@ -2,9 +2,9 @@ import * as Yup from 'yup';
 import {CreditValues} from "./types.ts";
 
 export const initialValues: CreditValues = {
-    amount: 10000,
-    term: 36,
-    rate: 17,
+    amount: 0,
+    term: 0,
+    rate: 0,
 };
 export const validationSchema = Yup.object<CreditValues>({
     amount: Yup.number().required('Required').positive('Must be positive'),
